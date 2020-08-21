9d1c667187f4f1fe5acf919fc5dfe937177ed9a8
Shindhi <diogoshindhinaka@gmail.com>
2020-08-20 23:17:55 -0300

    list of favorite foods

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -33,6 +33,13 @@ const Favorites: React.FC = () => {
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
       // Load favorite foods from api
+      const { data } = await api.get<Food[]>('favorites');
+      setFavorites(
+        data.map(food => ({
+          ...food,
+          formattedPrice: formatValue(food.price),
+        })),
+      );
     }
 
     loadFavorites();
